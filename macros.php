dd04fac1bb3b1e73d74e64e6046c87d7b2aa8597
ChrisRM <chris@hirvi.no>
2023-06-28 14:16:37 +0200

    Update

--- macros.php
+++ macros.php
@@ -6,8 +6,9 @@
  * as the base macro class \Jet_Engine_Base_Macros is not available before that action;
  * after it - all macros are registered already
  */
-require_once( __DIR__ . '/macros/macro-cookie.php' );
 
 add_action( 'jet-engine/register-macros', function(){
+    require_once( __DIR__ . '/macros/macro-cookie.php' );
+
     new Macro_CookieValue();
 });
